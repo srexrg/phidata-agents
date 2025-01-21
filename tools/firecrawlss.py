be6e7ab835ecaf4bacc3487dd4d9c5fda46ca34d
srexrg <sreeragp276@gmail.com>
2025-01-22 00:03:53 +0530

    update

---
+++ tools/firecrawlss.py
@@ -0,0 +1,32 @@
+from phi.agent import Agent
+from phi.knowledge.json import JSONKnowledgeBase
+from phi.vectordb.pgvector import PgVector
+
+
+
+knowledge_base = JSONKnowledgeBase(
+    path="tools/docs.json",
+    vector_db=PgVector(
+        table_name="agent_framweork",
+        db_url="postgresql+psycopg://ai:ai@localhost:5532/ai",
+    ),
+)
+
+
+agent = Agent(
+    name="DBAgent",
+    knowledge=knowledge_base,
+    search_knowledge=True,
+    markdown=True,
+    instructions=[
+        "Use the knowledgebase to provide information.",
+        "Search for relevant information in the knowledgebase.",
+        "Provide clear and concise responses.",
+        "If no information is found, say so.",
+    ],
+    show_tool_calls=True,
+)
+agent.knowledge.load(recreate=False)
+
+
+agent.print_response("Give me 3 names of 3 web3 frameworks name ? ", markdown=True)
