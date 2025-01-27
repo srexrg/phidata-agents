98e4f500e6c92655d6da7b7a578ce94a2426b39b
srexrg <sreeragp276@gmail.com>
2025-01-27 16:34:00 +0530

    add cal agent

--- agents/personal_agent.py
+++ agents/personal_agent.py
@@ -3,10 +3,13 @@ from phi.tools.duckduckgo import DuckDuckGo
 from phi.tools.yfinance import YFinanceTools
 from phi.tools.hackernews import HackerNews
 from phi.model.openai import OpenAIChat
+from phi.tools.calcom import CalCom
 from phi.knowledge.pdf import PDFUrlKnowledgeBase
 from phi.vectordb.pgvector import PgVector
 from phi.tools.crawl4ai_tools import Crawl4aiTools
 from phi.tools.github import GithubTools
+from datetime import datetime
+
 
 db_url = "postgresql+psycopg://ai:ai@localhost:5532/ai"
 
@@ -31,6 +34,26 @@ legal_agent = Agent(
     ],
     show_tool_calls=True,
 )
+
+calendar_agent = Agent(
+    name="Calendar Assistant",
+    instructions=[
+        f"You're scheduing assistant. Today is {datetime.now()}.",
+        "You can help users by:",
+        "- Finding available time slots",
+        "- Creating new bookings",
+        "- Managing existing bookings (view, reschedule, cancel) ",
+        "- Getting booking details",
+        "- IMPORTANT: In case of rescheduling or cancelling booking, call the get_upcoming_bookings function to get the booking uid. check available slots before making a booking for given time",
+        "Always confirm important details before making bookings or changes.",
+    ],
+    model=OpenAIChat(id="gpt-4o"),
+    tools=[CalCom(user_timezone="Asia/Kolkata")],
+    show_tool_calls=True,
+    markdown=True,
+)
+
+
 
 personal_assistant = Agent(
     name="Personal Assistant",
@@ -57,9 +80,9 @@ personal_assistant = Agent(
 )
 
 agent_team = Agent(
-    team=[legal_agent, personal_assistant],
+    team=[legal_agent, personal_assistant, calendar_agent],
     instructions=[
-        "Use the LegalAdvisor for legal questions and the PersonalAssistant for general tasks.",
+        "Use the LegalAdvisor for legal questions,PersonalAssistant for general tasks and CalendarAssistant for scheduling tasks.",
         "Always provide sources and use markdown formatting for better readability.",
     ],
     show_tool_calls=True,
@@ -75,6 +98,9 @@ agent_team = Agent(
 #     "Search for the repo local-loop and get the details of the repo where the user is srexrg", stream=True
 # )
 
+# agent_team.print_response(
+#     "What are the penalties for spoofing Email Address ?", stream=True
+# )
 agent_team.print_response(
-    "What are the penalties for spoofing Email Address ?", stream=True
+    "Schedule a meeting on 28th January 2025 at 10:00 AM IST and add sreerag as the attendee mail id is sreeragp777@gmail.com", stream=True
 )
